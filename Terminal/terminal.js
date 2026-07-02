fcf9daa1fb97257ac8fbc694cb30ab0095dc2ca7
Daniel Hoffmaster <daniel.m.hoffmaster@gmail.com>
2026-07-02 12:35:33 -0500

    bones of terminal setup

--- Terminal/terminal.js
+++ Terminal/terminal.js
@@ -2,30 +2,47 @@ function drawLineBetweenDivs(element1, element2){
 
 }
 
-function updateMap(){
-    
+function updateVisualMap(map){
+    for(entry in map.entries()){
+        for(connection in map.value){
+            drawLineBetweenDivs(entry.value, connection);
+        }
+    }
 }
-function loadProjects(){
-
+async function loadProjects(){
+    //create node for all the projects on first pass
+    //add edges on second pass
 }
 
 function calculateRouteToProject(target){
-
+    //breadth first search from start
 }
 
 function highlightRoute(route){
+    //set class of the svg for each edge to highlighted
+}
+
+function routeToString(route){
 
 }
 
-function routeToString(){
-
-}
-
-function setRouteText(){
+function setRouteText(route){
 
 }
 
 function projectQuickTravel(project){
+
+}
+
+function rankSearchResults(context){
+    //get levenshtein distance to each name
+}
+
+function displaySearchResults(){
+
+}
+
+function levenshteindistance(a, b){
 
 }
 
@@ -36,3 +53,6 @@ function projectQuickTravel(project){
     - Description
     - Type(domain or project)
 */
+
+let projects = new Map();
+loadProjects().then(function(map){projects = map;}, function(){alert("Failed to load projects, website will not display as intended!")});
