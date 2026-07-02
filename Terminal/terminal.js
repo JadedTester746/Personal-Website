4de4e5d3a736a2181844b8fabecec1a8cd29a789
Daniel Hoffmaster <daniel.m.hoffmaster@gmail.com>
2026-07-02 11:58:04 -0500

    Started working on computer terminal

---
+++ Terminal/terminal.js
@@ -0,0 +1,38 @@
+function drawLineBetweenDivs(element1, element2){
+
+}
+
+function updateMap(){
+    
+}
+function loadProjects(){
+
+}
+
+function calculateRouteToProject(target){
+
+}
+
+function highlightRoute(route){
+
+}
+
+function routeToString(){
+
+}
+
+function setRouteText(){
+
+}
+
+function projectQuickTravel(project){
+
+}
+
+/*
+    Project JSON
+    - Name
+    - Href
+    - Description
+    - Type(domain or project)
+*/
