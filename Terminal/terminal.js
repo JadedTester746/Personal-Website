2bfb1f34beb5a2cc936315e2e1b2c32c67a40339
Daniel Hoffmaster <daniel.m.hoffmaster@gmail.com>
2026-07-22 23:06:49 -0500

    Javascript for loading projects and finding routes implemented

--- Terminal/terminal.js
+++ Terminal/terminal.js
@@ -13,12 +13,44 @@ function updateVisualMap(map){
     }
 }
 async function loadProjects(){
-    //create node for all the projects on first pass
-    //add edges on second pass
+    const indexFile = await fetch("../Projects/index.json");
+    const projectNames = JSON.parse(await indexFile.text());
+
+    console.log(projectNames.projects);
+
+    for(let i = 0; i < projectNames.projects.length; i++){
+        const curr = JSON.parse(await (await fetch(projectNames.projects[i])).text());
+        console.log(curr)
+        projects.set(curr, new Set());
+    }
 }
 
 function calculateRouteToProject(target){
-    //breadth first search from start
+    let queue = [];
+
+    let seen = new Set();
+    queue.push({node: start, prev: null});
+    let out = null;
+    while(queue.length> 0){
+        let node = queue.shift();
+        seen.add(node.node);
+        if(node.node == target){out = node;}
+        else{
+            for(let n in map.get(node.node)){
+                if(!seen.contains(n)){queue.push({node: n, prev:node});}
+            }
+        }
+    }
+
+    let route = [];
+    while(out.prev != null){
+        route.push(out.node);
+        out = out.prev;
+    }
+    route.push(start);
+    route.reverse();
+    return route;
+    
 }
 
 function highlightRoute(route){
@@ -26,27 +58,61 @@ function highlightRoute(route){
 }
 
 function routeToString(route){
-
+    let routeString = "";
+    for(let i = 0; i < route.length; i++){
+        routeString += route[i].name + "->";
+    }
+    return routeString.substring(0, routeString.length - 2);
 }
 
 function setRouteText(route){
-
+    
+    document.getElementById("routetext").innerHTML = `Route: ${routeToString(route)}`;
 }
 
 function projectQuickTravel(project){
-
+    window.open(project.href, '_blank');
 }
 
 function rankSearchResults(context){
-    //get levenshtein distance to each name
+    let out = new Array();
+    let i = 0;
+    for(let entry in projects.entries){
+        out[i] = `{object:${entry.key}, distance:${levenshteindistance(context, entry.key.name)}}`;
+    }
+    out.sort(function(a, b){return a.distance-b.distance;});
+    return out;
 }
 
-function displaySearchResults(){
-
+function displaySearchResults(results){
+    //Set top results
 }
 
 function levenshteindistance(a, b){
+    let m = a.length;
+    let n = b.length;
 
+    const dp = new Array(m + 1).fill(null).map(() => new Array(n + 1).fill(0));
+
+    for(let i = 0; i <= m; i++){
+        dp[i][0] = i;
+    }
+
+    for(let i = 0; i <= n; i++){
+        dp[0][i] = i;
+    }
+
+    for(let i = 1; i <= m; i++){
+        for(let j = 1; j <= n; j++){
+            if(a[i-1] === b[j-1]){
+                dp[i][j] = dp[i-1][j-1];
+            }
+            else{
+                dp[i][j] = 1+ Math.min(dp[i][j-1], Math.min(dp[i-1][j], dp[i-1][j-1]));
+            }
+        }
+    }
+    return dp[m][n];
 }
 
 /*
@@ -55,9 +121,11 @@ function levenshteindistance(a, b){
     - Href
     - Description
     - Type(domain or project)
+    - Connections array
 */
 
 let projects = new Map();
-loadProjects().then(function(map){projects = map;}, function(){alert("Failed to load projects, website will not display as intended!")});
+const start = null;
+loadProjects().then(function(map){alert(projects.toString());}, function(){alert("Failed to load projects, website will not display as intended!")});
 
 
